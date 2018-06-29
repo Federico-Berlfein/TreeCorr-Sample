883726bb9d112d2a2b07d78ec859fccc48af9fc5
Federico-Berlfein <37029293+Federico-Berlfein@users.noreply.github.com>
2018-06-29 17:25:40 -0400

    Update randoms_richness_test.py

--- randoms_richness_test.py
+++ randoms_richness_test.py
@@ -7,33 +7,35 @@ from .catalog_reader import RMReader
 
 class RandomsRicnhessTest(BaseValidationTest):
     def __init__(self, **kwargs):
-        self.quantities_needed = ["catalogName", "random"]
+        self.quantities_needed = ["catalogName,LAMBDA_CHISQ","catalogName,Z_LAMBDA", "random, ZTRUE", "random,AVG_LAMBDAOUT"]
     
     def run(self, configpath, outputdir):
         reader = RMReader(configpath)
         quantities = reader.get_quantities(self.quantities_needed)
-        catalog = quantities["catalogName"]
-        random = quantities["random"]
-        cat = Table.read(catalog)
-        rand= Table.read(random)
-        data = [cat, rand]
-        self.makeplot(data, outputdir)
+        cat_lambda = quantities["catalogName,LAMBDA_CHISQ"]
+        cat_z = quantities["catalogName,Z_LAMBDA"]
+        rand_lambda = quantities["random,AVG_LAMBDAOUT"]
+        rand_z = quantities["random, ZTRUE"]
+        cat_data = [cat_lambda, cat_z]
+        rand_data =  [rand_lambda, rand_z]
+        self.makeplot(cat_data, rand_data, outputdir)
         return TestResult(inspect_only=True)
-        
-    def makeplot(self, data, outputdir):
-        zmin = [0.1, 0.4, 0.7]
-        zmax = [0.4, 0.7, 1]
-        def rich(catalog, zmin, zmax):
-            z = np.array(catalog['Z_LAMBDA'])
+    
+    def rich(self, data, zmin, zmax):
+            z = data[1] 
+            lambd = data[0]
             mask = ((z>zmin) & (z<zmax))
-            new_cat = catalog[mask]
-            cat_lambda = np.array(new_cat['LAMBDA_CHISQ_C'])
-            return (cat_lambda)
+            new_lambda = lambd[mask]
+            return (new_lambda)
+        
+    def makeplot(self, cat_data, rand_data, outputdir):
+         zmin = [0.1, 0.4, 0.7]
+         zmax = [0.4, 0.7, 1]
          f, ax = plt.subplots(1,3, figsize = (15,5))
          bi_space = np.linspace(0,150)
          for i in range (0,3):
-            cat = rich(data[0], zmin[i], zmax[i])
-            ran = rich(data[1], zmin[i], zmax[i])
+            cat = self.rich(cat_data, zmin[i], zmax[i])
+            ran = self.rich(rand_data, zmin[i], zmax[i])
             ax[i].hist(ran, bins = bi_space, color = 'orange', normed = True, histtype = 'step', lw = 2, label = 'randoms')
             ax[i].hist(cat, bins = bi_space, color = 'blue', normed = True, histtype = 'step', lw = 2, label= 'data')
             ax[i].set_xlim(0,150)
@@ -41,4 +43,4 @@ class RandomsRicnhessTest(BaseValidationTest):
             ax[i].set_xlabel ('Richness')
             ax[i].set_title('Richness for z:  ' + str(zmin[i]) + '-'+ str(zmax[i]))
          f.savefig(outputdir+"RandomsRichness.png")
-         f.close()
+         plt.close(f)
